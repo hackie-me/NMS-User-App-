c4cd65c58737a22674f22f4ab1bf873fc295991f
Student <hcollege0@gmail.com>
2023-02-23 10:41:05 +0530

    My Account Fragment display name from Token and set click listers on linear layout

--- app/src/main/java/com/nms/user/fragments/AccountFragment.kt
+++ app/src/main/java/com/nms/user/fragments/AccountFragment.kt
@@ -4,15 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.nms.user.R
+import com.nms.user.utils.Helper
 
 class AccountFragment : Fragment() {
+
+    private lateinit var linearLayoutGroupItemMyAccount : LinearLayout
+    private lateinit var linearLayoutGroupItemMyOrder : LinearLayout
+    private lateinit var linearLayoutGroupItemBilling : LinearLayout
+    private lateinit var linearLayoutGroupItemFaq : LinearLayout
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        // initialize views
+        initializeViews()
+
+        // initialize user data
+        initializeUserData()
+
+        // set click listener
+        setClickListener()
+    }
+
+    // Function to initialize views
+    private fun initializeViews(){
+        linearLayoutGroupItemMyAccount = view?.findViewById(R.id.linearRowgroupitemPrivateAccount)!!
+        linearLayoutGroupItemMyOrder = view?.findViewById(R.id.linearRowgroupitemMyOrders)!!
+        linearLayoutGroupItemBilling = view?.findViewById(R.id.linearRowgroupitemBilling)!!
+        linearLayoutGroupItemFaq = view?.findViewById(R.id.linearRowgroupitemFaq)!!
+    }
+    private fun initializeUserData(){
+        val userFullName = let { Helper.getDataFromToken(requireContext(), "full_name") }
+        view?.findViewById<TextView>(R.id.txtUserName)?.text = "Hi, $userFullName"
+    }
+
+    // Function to set click listener for Linear layouts
+    private fun setClickListener(){
+
+        // My Account
+        linearLayoutGroupItemMyAccount.setOnClickListener {
+            Helper.showToast(requireContext(), "My Account")
+        }
+
+        // My Order
+        linearLayoutGroupItemMyOrder.setOnClickListener {
+            Helper.showToast(requireContext(), "My Order")
+        }
+
+        // Billing
+        linearLayoutGroupItemBilling.setOnClickListener {
+            Helper.showToast(requireContext(), "Billing")
+        }
+
+        // FAQ
+        linearLayoutGroupItemFaq.setOnClickListener {
+            Helper.showToast(requireContext(), "FAQ")
+        }
     }
 }
